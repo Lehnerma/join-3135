d02c8cae1e421aab4d3d988802004b27aeec0b5e
Max Lehner <150380803+Lehnerma@users.noreply.github.com>
2026-04-09 08:17:42 +0200

    change the structure of the dialogs

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -15,7 +15,7 @@ function initBtn() {
 }
 
 function generateId() {
-  return Date.now().toString(36) + Math.random().toString(36);
+  return (Date.now().toString(36) + Math.random().toString(36)).substring(0, 6);
 }
 
 function openDialog(el) {
