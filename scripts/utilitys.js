63b4a56d0707a5298344c0bd56d8701388e44fc1
Max Lehner <150380803+Lehnerma@users.noreply.github.com>
2026-04-16 23:22:44 +0200

    updatet the auth funciton - to fetch the actual user ids in the firebase - to check iif the user id exist.

--- scripts/utilitys.js
+++ scripts/utilitys.js
@@ -1,6 +1,20 @@
 function checkAuth() {
-  if (!sessionStorage.getItem("user_id")) {
+  if (checkUserId(sessionStorage.getItem("user_id"))) {
     window.location.href = "../index.html";
+  }
+}
+
+async function checkUserId(id) {
+  try {
+    const RESPONS = await fetch("https://join-3135-default-rtdb.europe-west1.firebasedatabase.app/users.json");
+    if (!RESPONS.ok) {
+      throw new Error(RESPONS.status);
+    }
+    const RESULT = RESPONS.json;
+    const ALLIDS = Object.values(RESULT).map((items) => items.id);
+    return Object.values(RESULT).some((item) => item.id === id);
+  } catch (er) {
+    console.error(er);
   }
 }
 
