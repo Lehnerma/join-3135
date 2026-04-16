53be218dc5b08e70dc474d4a4488a98a45cc9082
Max Lehner <150380803+Lehnerma@users.noreply.github.com>
2026-04-17 00:27:15 +0200

    add docs to the utilitys.js

--- scripts/utilitys.js
+++ scripts/utilitys.js
@@ -1,9 +1,17 @@
+/**
+ * Check if the User can enter the side or not
+ */
 function checkAuth() {
   if (checkUserId(sessionStorage.getItem("user_id"))) {
     window.location.href = "../index.html";
   }
 }
 
+/**
+ *Function to check if the user id is available in the backend
+ * @param {string} id
+ * @returns Bool if the id is true ore false
+ */
 async function checkUserId(id) {
   try {
     const RESPONS = await fetch("https://join-3135-default-rtdb.europe-west1.firebasedatabase.app/users.json");
