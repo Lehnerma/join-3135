9d37788a830c0d30a3015c79e450f835a9c27210
Max Lehner <150380803+Lehnerma@users.noreply.github.com>
2026-04-29 10:52:23 +0200

    Finish the styling for the task cards and begin implementing the logic to nics code

--- scripts/templates.js
+++ scripts/templates.js
@@ -2,8 +2,8 @@
  * The template for the subtasks
  * @returns the html code for creat a subtask
  */
-function getSubtaskTemplate(title, index){
-    return `
+function getSubtaskTemplate(title, index) {
+  return `
     <span id="subtask${index}" class="subtask-text">${escapeHtml(title)}</span>
     <div class="subtask-item--btns">
       <button type="button" class="btn--subtask btn--delete">
@@ -13,11 +13,42 @@ function getSubtaskTemplate(title, index){
       <button type="button" class="btn--subtask btn--edit">
         <img src="../assets/img/icons/subtask/edit.svg" alt="edit" />
       </button>
-    </div>`
+    </div>`;
 }
 
-function getNoTasksTemplate(status){
+function getNoTasksTemplate(status) {
   return `
     <li class="board-task no-task">No tasks ${status}</li>
-  `
+  `;
+}
+
+function getTaskCard() {
+  return `
+            <li class="task" draggable="true">
+              <article class="task--card">
+                <header>
+                  <span class="task--category-label user-story">User Story</span>
+                </header>
+                <section class="task--content" aria-label="task content">
+                  <div class="task--title-wrapper">
+                    <h3 class="task--title">title</h3>
+                    <p class="task--description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaque.</p>
+                  </div>
+                  <!-- progress -->
+                  <div class="subtask--progress">
+                    <progress class="subtask--progressbar" max="2" value="1"></progress>
+                    <span class="subtask--description">1/2 Subtasks</span>
+                  </div>
+                </section>
+                <footer class="task--footer">
+                  <ul class="task--assignees" aria-label="Zugewiesene Personen">
+                    <li class="assignee">
+                      <abbr class="assignee--initials" title="Anna Müller">AM</abbr>
+                    </li>
+                  </ul>
+                  <img src="../assets/img/icons/prio/low.svg" alt="low priority" class="prio-icon">
+                </footer>
+              </article>
+            </li>
+  `;
 }
