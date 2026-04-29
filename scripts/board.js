d99f2922d90250176e52a632302314643b1efa46
Max Lehner <150380803+Lehnerma@users.noreply.github.com>
2026-04-29 11:41:55 +0200

    beginn renderBoard function to change it to a arrow function for cleaner code

--- scripts/board.js
+++ scripts/board.js
@@ -4,105 +4,39 @@ let TASKS = [];
 
 function initBoard() {
   //NO_TASKS();
-  loadTasksFirebase();
+  loadTasksFromFirebase();
+}
+
+async function loadTasksFromFirebase() {
+  try {
+    const RESPONSE = await fetch(TASK_URL);
+    if (!RESPONSE.ok) {
+      throw new Error(`loading task faild: ${RESPONSE.status}`);
+    }
+    const RESULT = await RESPONSE.json();
+    sessionStorage.setItem('tasks', JSON.stringify(RESULT))
+    renderBoard(RESULT)
+  } catch (er) {
+    console.error(er);
+  }
 }
 
 // function loadTasks() {
-//   return JSON.parse(localStorage.getItem("tasks")) || [];
+//   return JSON.parse(sessionStorage.getItem("tasks")) || [];
 // }
 
-// function renderBoard() {
-//   let tasks = loadTasks();
+function renderBoard(tasks){
+  const TASKS = tasks;
+  // for (let t = 0; t < TASKS.length; t++) {
+  //   console.log(TASKS[t]);
+    
+  // }
+  TASKS.forEach((task) => {
+    
+    
+  })
+}
 
-//   let todo = document.getElementById("todo-list");
-//   let inProgress = document.getElementById("inprogress-list");
-//   let feedback = document.getElementById("feedback-list");
-//   let done = document.getElementById("done-list");
-
-//   todo.innerHTML = "";
-//   inProgress.innerHTML = "";
-//   feedback.innerHTML = "";
-//   done.innerHTML = "";
-
-//   function getProgress(subtasks) {
-//     let done = subtasks.filter((s) => s.done).length;
-//     let total = subtasks.length;
-//     return { done, total };
-//   }
-
-//   for (let i = 0; i < tasks.length; i++) {
-//     let task = tasks[i];
-
-//     let progress = getProgress(task.subtasks || []);
-//     let percent = progress.total ? (progress.done / progress.total) * 100 : 0;
-
-// let card = `
-//                             <div class="task-card">
-                            
-//                                 <div class="category-label">${task.category}</div>
-//                                 <div class="task-title">${task.title}</div>
-//                                 <div class="task-description">${task.description}</div>
-                            
-//                                 ${
-//                                   progress.total > 0
-//                                     ? `
-//                                 <div class="subtask-progress" title="${progress.done} of ${progress.total} subtasks done">
-//                                     <div class="progress-bar-bg">
-//                                         <div class="progress-bar-fill" style="width:${percent}%"></div>
-//                                     </div>
-//                                     <span>${progress.done}/${progress.total} Subtasks</span>
-//                                 </div>
-//                                 `
-//                                     : ""
-//                                 }
-                            
-//                                 <div class="subtask-list">
-//                                     ${(task.subtasks || [])
-//                                       .map(
-//                                         (sub, subIndex) => `
-//                                         <div class="subtask-item" onclick="toggleSubtask(${i}, ${subIndex})">
-//                                             ${sub.done ? "✔" : "❌"} ${sub.title}
-//                                         </div>
-//                                     `,
-//                                       )
-//                                       .join("")}
-//                                 </div>
-                            
-//                                 <div class="task-footer">
-//                                     <span>${task.priority}</span>
-//                                 </div>
-                            
-//                             </div>
-//                             `;
-
-//     if (task.status === "todo") {
-//       todo.innerHTML += card;
-//     }
-
-//     if (task.status === "inProgress") {
-//       inProgress.innerHTML += card;
-//     }
-
-//     if (task.status === "feedback") {
-//       feedback.innerHTML += card;
-//     }
-
-//     if (task.status === "done") {
-//       done.innerHTML += card;
-//     }
-//   }
-// }
-
-// renderBoard();
-
-// function toggleSubtask(taskIndex, subIndex) {
-//   let tasks = JSON.parse(localStorage.getItem("tasks"));
-
-//   tasks[taskIndex].subtasks[subIndex].done = !tasks[taskIndex].subtasks[subIndex].done;
-
-//   localStorage.setItem("tasks", JSON.stringify(tasks));
-//   renderBoard();
-// }
 
 function renderNoTasksElemt(status) {
   let LIST = document.getElementById(status + "_list");
@@ -117,15 +51,4 @@ function NO_TASKS() {
   });
 }
 
-async function loadTasksFirebase() {
-  try {
-    const RESPONSE = await fetch(TASK_URL);
-    if (!RESPONSE.ok) {
-      throw new Error(`loading task faild: ${RESPONSE.status}`);
-    }
-    const RESULT = await RESPONSE.json();
-    sessionStorage.setItem('tasks', JSON.stringify(RESULT))
-  } catch (er) {
-    console.error(er);
-  }
-}
+
--- scripts/templates.js
+++ scripts/templates.js
@@ -22,33 +22,37 @@ function getNoTasksTemplate(status) {
   `;
 }
 
-function getTaskCard() {
+function getTaskCardTemplet(title, description) {
   return `
-            <li class="task" draggable="true">
-              <article class="task--card">
-                <header>
-                  <span class="task--category-label user-story">User Story</span>
-                </header>
-                <section class="task--content" aria-label="task content">
-                  <div class="task--title-wrapper">
-                    <h3 class="task--title">title</h3>
-                    <p class="task--description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaque.</p>
-                  </div>
-                  <!-- progress -->
-                  <div class="subtask--progress">
-                    <progress class="subtask--progressbar" max="2" value="1"></progress>
-                    <span class="subtask--description">1/2 Subtasks</span>
-                  </div>
-                </section>
-                <footer class="task--footer">
-                  <ul class="task--assignees" aria-label="Zugewiesene Personen">
-                    <li class="assignee">
-                      <abbr class="assignee--initials" title="Anna Müller">AM</abbr>
-                    </li>
-                  </ul>
-                  <img src="../assets/img/icons/prio/low.svg" alt="low priority" class="prio-icon">
-                </footer>
-              </article>
-            </li>
-  `;
+<li class="task" draggable="true">
+  <article class="task--card">
+    <header>
+      <span class="task--category-label user-story">User Story</span>
+    </header>
+    <section class="task--content" aria-label="task content">
+      <div class="task--title-wrapper">
+        <h3 class="task--title">${title}}</h3>
+        <p class="task--description">${description}</p>
+      </div>
+      
+      <div class="subtask--progress">
+        <progress class="subtask--progressbar" max="2" value="1"></progress>
+        <span class="subtask--description">1/2 Subtasks</span>
+      </div>
+    </section>
+    <footer class="task--footer">
+      <ul class="task--assignees" aria-label="Zugewiesene Personen">
+        
+      </ul>
+      <img src="../assets/img/icons/prio/low.svg" alt="low priority" class="prio-icon">
+    </footer>
+  </article>
+</li>`;
+}
+
+function getTaskAssignToTemplet(fullName, initials) {
+  return `
+<li class="assignee">
+  <abbr class="assignee--initials" title="${fullName}">${initials}</abbr>
+</li>`;
 }
