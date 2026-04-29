425782aabdbeef7b3a2609a76ed58943be5c966d
Max Lehner <150380803+Lehnerma@users.noreply.github.com>
2026-04-29 08:58:36 +0200

    - fix the bg color in the content area of the sidebar. also make little improvements in the board functions.

--- scripts/board.js
+++ scripts/board.js
@@ -1,8 +1,10 @@
 const TASK_URL = "https://join-3135-default-rtdb.europe-west1.firebasedatabase.app/tasks" + ".json";
 const STATUS = ["todo", "progress", "feedback", "done"];
+let TASKS = [];
 
 function initBoard() {
   NO_TASKS();
+  loadTasksFirebase();
 }
 
 // function loadTasks() {
@@ -34,44 +36,44 @@ function initBoard() {
 //     let progress = getProgress(task.subtasks || []);
 //     let percent = progress.total ? (progress.done / progress.total) * 100 : 0;
 
-//     let card = `
-//                             <div class="task-card">
+let card = `
+                            <div class="task-card">
                             
-//                                 <div class="category-label">${task.category}</div>
-//                                 <div class="task-title">${task.title}</div>
-//                                 <div class="task-description">${task.description}</div>
+                                <div class="category-label">${task.category}</div>
+                                <div class="task-title">${task.title}</div>
+                                <div class="task-description">${task.description}</div>
                             
-//                                 ${
-//                                   progress.total > 0
-//                                     ? `
-//                                 <div class="subtask-progress" title="${progress.done} of ${progress.total} subtasks done">
-//                                     <div class="progress-bar-bg">
-//                                         <div class="progress-bar-fill" style="width:${percent}%"></div>
-//                                     </div>
-//                                     <span>${progress.done}/${progress.total} Subtasks</span>
-//                                 </div>
-//                                 `
-//                                     : ""
-//                                 }
+                                ${
+                                  progress.total > 0
+                                    ? `
+                                <div class="subtask-progress" title="${progress.done} of ${progress.total} subtasks done">
+                                    <div class="progress-bar-bg">
+                                        <div class="progress-bar-fill" style="width:${percent}%"></div>
+                                    </div>
+                                    <span>${progress.done}/${progress.total} Subtasks</span>
+                                </div>
+                                `
+                                    : ""
+                                }
                             
-//                                 <div class="subtask-list">
-//                                     ${(task.subtasks || [])
-//                                       .map(
-//                                         (sub, subIndex) => `
-//                                         <div class="subtask-item" onclick="toggleSubtask(${i}, ${subIndex})">
-//                                             ${sub.done ? "✔" : "❌"} ${sub.title}
-//                                         </div>
-//                                     `,
-//                                       )
-//                                       .join("")}
-//                                 </div>
+                                <div class="subtask-list">
+                                    ${(task.subtasks || [])
+                                      .map(
+                                        (sub, subIndex) => `
+                                        <div class="subtask-item" onclick="toggleSubtask(${i}, ${subIndex})">
+                                            ${sub.done ? "✔" : "❌"} ${sub.title}
+                                        </div>
+                                    `,
+                                      )
+                                      .join("")}
+                                </div>
                             
-//                                 <div class="task-footer">
-//                                     <span>${task.priority}</span>
-//                                 </div>
+                                <div class="task-footer">
+                                    <span>${task.priority}</span>
+                                </div>
                             
-//                             </div>
-//                             `;
+                            </div>
+                            `;
 
 //     if (task.status === "todo") {
 //       todo.innerHTML += card;
@@ -108,8 +110,22 @@ function renderNoTasksElemt(status) {
   LIST.innerHTML += getNoTasksTemplate(status);
 }
 
+// only for programming
 function NO_TASKS() {
   STATUS.forEach((s) => {
     renderNoTasksElemt(s);
   });
+}
+
+async function loadTasksFirebase() {
+  try {
+    const RESPONSE = await fetch(TASK_URL);
+    if (!RESPONSE.ok) {
+      throw new Error(`loading task faild: ${RESPONSE.status}`);
+    }
+    const RESULT = await RESPONSE.json();
+    TASKS.push(RESULT);
+  } catch (er) {
+    console.error(er);
+  }
 }
